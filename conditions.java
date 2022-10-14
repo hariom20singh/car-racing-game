2a1e9dfcb27f58bcbc90450c6546af6d95b181c5
anubhav-002 <105811036+anubhav-002@users.noreply.github.com>
2022-10-14 10:53:31 +0530

    Add files via upload

---
+++ conditions.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class conditions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+
+    }
+
+}
