02c31620646e729ed4707dbf0185ca2fad997af4
Abhiraj Singh Bhadoria <116302275+Abhirajxoo@users.noreply.github.com>
2022-10-29 22:06:15 +0530

    HACKTOBER FEST 2022
    HEY SIR
    PLZ ADD MY FILE TO YOUR REPO.
    THANK YOU

---
+++ Third.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+    int factorial=1;
+
+    for(int i=2;i<=n;i++){
+        factorial = factorial*i;
+   }
+   return factorial;
+}
+int main(){
+    int n;
+    cin>>n;
+
+    int ans = fact(n);
+    cout<<ans<<endl;
+    return 0;
+}
